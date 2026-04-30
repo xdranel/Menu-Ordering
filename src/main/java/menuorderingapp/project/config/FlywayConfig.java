1906e500aa738e90259e1b59d1445efdd664f4e5
xdranel <gendhiramona@gmail.com>
2026-05-01 00:03:19 +0700

    fix: bugs where conflict causes by database migration

---
+++ src/main/java/menuorderingapp/project/config/FlywayConfig.java
@@ -0,0 +1,18 @@
+package menuorderingapp.project.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public FlywayMigrationStrategy flywayRepairStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
